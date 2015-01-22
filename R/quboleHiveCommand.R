3971fd5c8396e7522ba5663f8f519354fdf3c952
Karandeep Johar <karandeepjohar@gmail.com>
2015-01-22 16:08:51 +0530

    published at CRAN

--- R/quboleHiveCommand.R
+++ R/quboleHiveCommand.R
@@ -1,11 +1,9 @@
-
-library(rPython)
-#setwd("~/Downloads/qds-sdk-py")
 # Load/run the main Python script
 #'  calls hivecommand_from_r and return the result
 #'  
 #'  \url{http://qubole.com}
 #'  \email{karandeepj@@qubole.com}
+#'  @import rPython
 #'  @param query A String
 #'  @param poll_interval A Number
 #'  @param sample_size A Number
@@ -19,14 +17,13 @@ library(rPython)
 #'  @examples
 #'  quboleHiveCommand("show tables")
 #'  @export
-#quboleHiveCommand<-function(query=NULL, script_location=NULL, macros=NULL , tags=NULL, sample_size=NULL, cluster_label=NULL, notify=NULL, name=NULL, poll_interval=15
-quboleHiveCommand<-function(...){ 
+quboleHiveCommand<-function(query = NULL, poll_interval = NULL, sample_size = NULL, macros = NULL, tags =NULL, cluster_label = NULL, notify = NULL, name = NULL, api_token = NULL){ 
     
     rPython::python.load(file.path(find.package("qds.sdk.R"),"exec","r-python.py" ))
     
-    #results<-python.call("hivecommand_from_r", query, script_location, macros , tags, sample_size, cluster_label, notify, name, poll_interval, api_token)
     
-    results<-rPython::python.call("hivecommand_from_r",...)
+    
+    results<-rPython::python.call("hivecommand_from_r", query, poll_interval, sample_size, macros, tags, cluster_label, notify, name, api_token)
   
     cat(results)
 }
--- exec/r-python.py
+++ exec/r-python.py
@@ -42,11 +42,16 @@ def hivecommand( queryString):
     
     return results 
 
-def hivecommand_from_r(query=None, poll_interval = os.getenv('QDS_POLL_INTERVAL'), sample_size=None, macros=None , tags=None, cluster_label=None, notify=None, name=None, api_token = os.getenv('QDS_API_TOKEN') ):
     #get value of api_url,
+def hivecommand_from_r(query = None, poll_interval = None , sample_size = None, macros = None , tags = None, cluster_label = None, notify = None, name = None, api_token = None ):
+    
     api_url = os.getenv('QDS_API_URL')
     api_version = os.getenv('QDS_API_VERSION')
-     
+    if poll_interval is None:
+        poll_interval = os.getenv('QDS_POLL_INTERVAL')
+    
+    if api_token is None:
+        api_token = os.getenv('QDS_API_TOKEN') 
     
     chatty = False
     verbose = False
@@ -59,46 +64,49 @@ def hivecommand_from_r(query=None, poll_interval = os.getenv('QDS_POLL_INTERVAL'
     #reconstruct the queryString for to be parsed by hivecommand.parse function
     if query is not None:
         queryString += " --query '%s' "%str(query)
-    #if script_location is not None:
-    #    queryString += " -f '%s' "%str(script_location)
+    
     if macros is not None:
         queryString += " --macros '%s' "%str(macros)
+    
     if tags is not None:
         queryString += " --tags '%s' "%str(tags)
+    
     if sample_size is not None:
         queryString += " --sample_size '%s' "%str(sample_size)
+    
     if cluster_label is not None:
         queryString += " --cluster-label '%s' "%str(cluster_label)
+    
     if notify is not None:
         queryString += " --notify '%s' "%str(notify)
+    
     if name is not None:
         queryString += " --name '%s' "%str(name)
 
-    #print "query is: "+query+"\n"
-    #query = str(query) #because r passes string as unicode
-    #arguments = str("")
-    #api_token = str(api_token)
-    
-    
     if chatty:
         logging.basicConfig(level=logging.DEBUG)
     elif verbose:
         logging.basicConfig(level=logging.INFO)
     else:
         logging.basicConfig(level=logging.WARN)
+    
     if api_token is None:
         sys.stderr.write("No API Token provided\n")
-        #usage(optparser)
+    
     if api_url is None:
         api_url = "https://api.qubole.com/api/"
+    
     if api_version is None:
         api_version = "v1.2"
+    
     if poll_interval is None:
         poll_interval = 5
+    
     if skip_ssl_cert_check is None:
         skip_ssl_cert_check = False
     elif skip_ssl_cert_check:
         log.warn("Insecure mode enabled: skipping SSL cert verification\n")
+    
     Qubole.configure(api_token=api_token,
             api_url=api_url,
             version=api_version,
@@ -118,7 +126,7 @@ def hivecommand_from_r(query=None, poll_interval = os.getenv('QDS_POLL_INTERVAL'
         
     except qds_sdk.exception.ParseError as e:
         sys.stderr.write("Error: %s\n" % str(e))
-        #sys.stderr.write("Usage: %s\n" % e.usage)
+        
         
     except Exception:
         traceback.print_exc(file=sys.stderr)
@@ -126,7 +134,6 @@ def hivecommand_from_r(query=None, poll_interval = os.getenv('QDS_POLL_INTERVAL'
 if __name__ =='main':
     print hivecommand_from_r()
 
-#Qubole.configure(api_token='7qpdaKMjWH4RzvsqGyG1ygcafqgBzsxr2d2ykz7SsBgTtY7cSs4uG9YdaBHHZWsU')
 
 
 
