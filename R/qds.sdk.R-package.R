4a108fad1086088ba5a9d4dc6fbc1b897b2c15fb
Karandeep Johar <karandeepjohar@gmail.com>
2015-01-22 13:51:13 +0530

    added man.pdf

---
+++ R/qds.sdk.R-package.R
@@ -0,0 +1,10 @@
+#' qds.sdk.R
+#'
+#' The qds.sdk.R package performs a simple call to qds-sdk-py to retriev results of a HiveCommand.
+#' @name qds.sdk.R
+#' @docType package
+#' @author Karandeep Johar: \email{karandeepj@@qubole.com} and
+#' Dinesh Rathi: \email{dineshr@@qubole.com}
+#' 
+#'
+NULL
