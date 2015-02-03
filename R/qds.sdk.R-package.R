4df7cd800a85fa66b6659ce507d6df4d406e96c5
Karandeep Johar <karandeepjohar@gmail.com>
2015-02-03 11:49:42 +0530

    Update qds.sdk.R-package.R

--- R/qds.sdk.R-package.R
+++ R/qds.sdk.R-package.R
@@ -1,10 +1,9 @@
 #' qds.sdk.R
 #'
 #' The qds.sdk.R package performs a simple call to qds-sdk-py to retriev results of a HiveCommand.
-#' @name qds.sdk.R
+#' @name qds
 #' @docType package
-#' @author Karandeep Johar: \email{karandeepj@@qubole.com} and
-#' Dinesh Rathi: \email{dineshr@@qubole.com}
+#' @author Qubole SDK Developer: \email{sdk-dev@@qubole.com} 
 #' 
 #'
 NULL
